293900de06d22f75741180ea22d43911694b0fdf
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2025-05-08 22:22:36 +0200

    working on session7

--- sesion7/read.md
+++ sesion7/read.md
@@ -30,3 +30,4 @@ void main ()
   }
   
 }
+Comando de compilación:  gcc -o main.c -I./include ./src/main.c ./src/misfunciones.c
--- sesion7/src/main.c
+++ sesion7/src/main.c
@@ -1,10 +1,31 @@
 #include "misfunciones.h"
 
-int main(){
 
+int main( int argc, char *argv[]){
+    int i;
+    
+    FILE * pf = NULL ;
 
+    printf("El programa  es:%s",argv[0]);
 
+    if( argc < 2){
 
+        printf("No encontraron argumentos en lines de comandos:(\n" );
+    } else {
+        for ( i = 0; i < argc; i++)
+        {
+           printf("Los arguemntos recojidos en lineas de comandos son :%s\n", argv[i]);
+
+        }
+    }
+
+    pf = fopen ("./miPrimerFichero.txt","w+"); // Open a TEXT file
+    if ( pf == NULL ){
+        printf ("Error opening %s.txt. Does it exist?\n", argv[argc - 1]);
+        exit(1);
+    }
+
+    fclose(pf);
 
     return 0;
 }
