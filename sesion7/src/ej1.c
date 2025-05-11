86bb4d03397dfe78923c66fa6916afc3748f847d
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2025-05-11 18:53:12 +0200

    working on session7

--- sesion7/src/ej1.c
+++ sesion7/src/ej1.c
@@ -1,80 +1,24 @@
 #include "misfunciones.h"
 
-int main( int argc, char *argv[]){
+int main(int argc, char *argv[])	//cabecera de main 
+{
+   if (argc!=2)
+   {  printf("ERROR: debes aportar un nombre de fichero.\n"); exit(0);  }
 
-   if( argc < 2) {     
+   //b) Crea una función que escriba en el fichero algo leido del teclado. Si el fichero no existe debe crearlo. 
+   //Si el fichero existe debe escribir al final del mismo.
+   creaFichero(argv[1]);	//llamamos a la función creaFichero() y le pasamos el nombre del fichero
 
-      printf("ERROR: debes aportar un nombre de fichero.\n"); 
-      exit(0); 
-      
-   } else {
-      creaFichero(argv[1]);
-      MuestraFichero(argv[1]);
-
-      
-      
-   }
-   return 0;
-}
-
-/* 
    //c) Crea una función que muestre el contenido del fichero por pantalla. Si el fichero no existe, emitir mensaje de error
-   XXXXXXXXXXXXXXXXXXXXXXX	//llamamos a la función muestraFichero() y le pasamos el nombre del fichero
+   MuestraFichero(argv[1]);	//llamamos a la función muestraFichero() y le pasamos el nombre del fichero
 
    //d) Crea una función a la que se le pasen dos caracteres. La función debe recorrer el fichero y cambiar el primer carácter por el segundo
-   XXXXXXXXX			//declaramos dos variables de tipo char (a y b)
-   printf("Dame la letra a buscar: ");
-   XXXXXXXXXXXXX		//leemos la letra en a
-   getchar();			//borramos el \n que queda en el buffer del teclado
-   printf("Dame letra de reemplazo: ");
-   XXXXXXXXXXXXX		//leemos la letra en b
-   XXXXXXXXXXXXXXXXX		//llamamos a la funcion cambiaLetras() y le pasamos el nombre del fichero y las dos letras a y b
-   XXXXXXXXXXXXXXXXXXXXX	//llamamos a la función muestraFichero para ver el nuevo contenido del mismo
+   char a, b;			//declaramos las dos variables de tipo char (a y b)
+   printf("Dame la letra a buscar: ");  
+   a = getchar(); 		//leemos en a la letra a buscar
+   getchar();			//borramos el \n del buffer del teclado
+   printf("Dame la letra de reemplazo: ");  
+   scanf("%c", &a); 		//leemos en b la letra de reemplazo (otra forma con scanf)
+   cambiaLetras(argv[1], a, b);	//llamamos a la funcion cambiaLetras() y le pasamos el nombre del fichero y las dos letras a y b
+   MuestraFichero(argv[1]);	//llamamos a la función muestraFichero para ver el nuevo contenido del mismo
 }
-
-
-//======================================================================================
-//FICHERO funciones7.c  (a guardar en la carpeta sesion7/src/):
-
-XXXXXXXXXXXXXXXXXXXXX		//incluimos nuestro fichero de cabecera, que contiene las declaraciones de nuestras funciones
-
-XXXXXXXXXXXXXXXXXXXXXXXXXX	//cabecera de la función creaFichero, recibe una cadena nombreFich con el nombre del fichero
-{
-   XXXXXXXXX				//declaramos un puntero a FILE pf
-   XXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//llamamos a fopen para abrir el fichero en modo "append" (a)
-   XXXXXXXXXXXXXXX			//si se ha producido un error de apertura de fichero
-   {  printf("ERROR de apertura del fichero %s\n", nombreFich);
-      return;				//salimos de la función
-   }
-   XXXXXXXXXXXXXXXX			//declaramos una cadena de 100 caracteres
-   printf("Dame un texto a grabar, seguido de Enter: ");	
-   XXXXXXXXXXXXXXXXXXXXXXX		//leemos por teclado una frase para la cadena (con fgets - stdin)
-   XXXXXXXXXXXXXXX			//grabamos la cadena en el fichero (fputs)
-   XXXXXXXXXXXXXXX			//si hay error de grabación
-      printf("ERROR al grabar en el fichero.\n");
-   XXXXXXXXXX				//cerramos el fichero
-}
-
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//cabecera de la función muestraFichero, recibe una cadena nombreFich con el nombre del fichero
-{
-   XXXXXXXXX				//declaramos un puntero a FILE pf
-   XXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//llamamos a fopen para abrir el fichero en modo "read" (r)
-   XXXXXXXXXXXXXXX			//si se ha producido un error de apertura de fichero
-   {  printf("ERROR: el fichero %s no existe.\n", nombreFich);	//informamos por pantalla que el fichero no existe
-      XXXXXXX				//salimos de la función
-   }
-   XXXXXXXXXXXXXX			//declaramos una cadena de 100 caracteres
-   printf("El contenido del fichero %s es:\n", nombreFich);
-   XXXXXXXXXXXXXXXXXXXXXXX		//leemos la primera frase del fichero en la cadena (fgets)(hasta el \n del fichero)
-   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//mientras no sea el fin del fichero y no se produzca un error de lectura
-   {
-      XXXXXXXXXXXXXXXXXXXX		//imprimimos la cadena en pantalla
-      XXXXXXXXXXXXXXXXXXXXX		//leemos la siguiente frase del fichero en la cadena (fgets)(hasta el \n)
-   }
-   XXXXXXXXXXXXXXXXXXX			//si se ha producido un error de lectura
-      printf("ERROR al leer el fichero %s.\n", nombreFich);	//imprimimos mensaje de error en pantalla
-   XXXXXXXXXX				//cerramos el fichero
-
-
-
-*/
--- sesion7/src/misfunciones.c
+++ sesion7/src/misfunciones.c
@@ -43,71 +43,38 @@ void MuestraFichero(char *NombreFichero){
 
 }
 
-void cambiaLetras(char *NombreFichero, char a, char b){
+void cambiaLetras(char *NombreFichero, char a, char b) {
 
-   FILE * fp;
-   int i;
-   fp = fopen("./text.txt","r");
+   FILE *fp;
+   fp = fopen(NombreFichero, "w+"); // Apertura en modo lectura y escritura
 
-   if( fp != NULL){
-      char cadAux[1000];
-      while (!ferror(fp) && !feof(fp)) {
-         for( i = 0; i < sizeof(cadAux); i++){
-            if( cadAux[i] == 'a'|| cadAux[i] == 'e'|| cadAux[i] == 'i'|| cadAux[i] == 'o'|| cadAux[i] == 'u' ){
-               cadAux[i] == a;
-               a == b;
-            }
-         }
-      }
-
-   } else {
-
-      printf("Error al abrir el fichero %s", NombreFichero);
+   if (fp == NULL) {
+      printf("Error al abrir el fichero %s\n", NombreFichero);
       exit(1);
    }
 
+   char cadena[100];
+   char cadenaTotal[1000] = "";
 
-
-
-
-}	
-
-
-
-/*
-{
-   XXXXXXX				//declaramos el puntero a FILE pf
-   XXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//llamamos a fopen para abrir el fichero en modo "r+"
-   XXXXXXXXXXXXXXX			//si se ha producido un error de apertura
-   {  printf("ERROR: el fichero %s no existe.\n", nombreFich); XXXXXXXXX  }	//mensaje de error y salimos de la función
-   XXXXXXXXXXXXXXXXX			//declaramos una cadena de 100 caracteres para leer una frase
-   char cadenaTotal[1000] = "";		//declaramos una cadenaTotal de 1000 caracteres donde concatenaremos todas las frases
-					//y la inicializamos a cadena vacía
-   XXXXXXXXXXXXXXXXXXXXXXX		//leemos la primera frase del fichero en la cadena (fgets)(lee hasta el \n)
-   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//mientras no sea el fin del fichero y no se produzca un error de lectura
-   {
-      XXXXXXXXXXXXXXXXXXXXXXXXXXXXX	//concatenamos la cadena al final de cadenaTotal (strcat)
-      XXXXXXXXXXXXXXXXXXXXXX		//leemos la siguiente frase del fichero en la cadena (fgets)(lee hasta el \n)
+   // Lectura del fichero correctamente
+   while (fgets(cadena, sizeof(cadena), fp) != NULL) {
+      strcat(cadenaTotal, cadena);
    }
-   XXXXXXXXXXXXXXXXXX			//si se ha producido un error de lectura del fichero
-      printf("ERROR al leer el fichero %s.\n", nombreFich);	//imprimimos mensaje de error
-   XXXX					//si no
-   {  
-      XXXXXXXXXXXXXXXXXXXXXXXXXXXX	//bucle for para acceder a cada letra de cadenaTotal
-      {
-         XXXXXXXXXXXXXXXXXXXX		//si la letra cadenaTotal[i] es igual a la de la variable "a"
-            XXXXXXXXXXXXXXXX		//cambiamos esa letra por la variable "b"
-      }
-      XXXXXXXXXXXXX			//movemos el apuntador de L/E al principio del fichero (rewind)
-      XXXXXXXXXXXXXXXXXXXXXX		//grabamos toda la cadenaTotal en el fichero (fputs)
-      XXXXXXXXXXXXXXX			//si se ha producido error al grabar
-         printf("ERROR al grabar el fichero %s.\n", nombreFich);	//imprimimos mensaje de error
-      XXXX				//si no
-         printf("El fichero %s ha sido modificado\n", nombreFich);
+
+   // Reemplazo de caracteres
+   for (int i = 0; cadenaTotal[i] != '\0'; i++) {
+      if (cadenaTotal[i] == a)
+         cadenaTotal[i] = b;
    }
-   XXXXXXXXXX				//cerramos el fichero
+
+   // Reescritura del fichero
+   rewind(fp);
+   fputs(cadenaTotal, fp);
+
+   if (ferror(fp))
+      printf("ERROR al grabar el fichero %s.\n", NombreFichero);
+   else
+      printf("El fichero ha sido modificado correctamente.\n");
+
+   fclose(fp);
 }
-
-
-*/
-
